3872cbf8b10fb1f91f76eb3e65d9a9e1f1f2f467
Harikrishnan <harisgkrishnan@yahoo.com>
2024-05-12 19:12:20 +0530

    if this changes aplly : data from sender(webhook related changes) will save in db

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -9,7 +10,6 @@ import (
 	dbconfig "whatbot/dbConfig"
 	"whatbot/model"
 	"whatbot/webhook"
-	"encoding/json"
 
 	_ "github.com/lib/pq"
 )
@@ -33,47 +33,79 @@ func main() {
 	StartHTTPServer(db)
 }
 
-func webhookHandler(w http.ResponseWriter, r *http.Request, payload webhook.WebhookPayload) {
+func webhookHandler(w http.ResponseWriter, r *http.Request, db *sql.DB, payload webhook.WebhookPayload) {
 	if payload.Object != "" {
-		if len(payload.Entry) > 0 && len(payload.Entry[0].Changes) > 0 && len(payload.Entry[0].Changes[0].Value.Messages) > 0 {
-			phoneNumberID := payload.Entry[0].Changes[0].Value.Messages[0].Metadata.PhoneNumberID
-			from := payload.Entry[0].Changes[0].Value.Messages[0].From
-			msgBody := payload.Entry[0].Changes[0].Value.Messages[0].Text.Body
+		var messageResponses []map[string]interface{}
+		var metaDataResponses []map[string]interface{}
+		var contactResponses []map[string]interface{}
 
-			fmt.Println("Phone number:", phoneNumberID)
-			fmt.Println("From:", from)
-			fmt.Println("Body:", msgBody)
+		for _, entry := range payload.Entry {
+			for _, change := range entry.Changes {
+				if len(change.Value.Messages) > 0 {
+					phoneNumberID := change.Value.Metadata.PhoneNumberID
+					DisplayPhoneNumber := change.Value.Metadata.DisplayPhoneNumber
+					from := change.Value.Messages[0].From
+					msgBody := change.Value.Messages[0].Text.Body
+					id := change.Value.Messages[0].ID
+					timeStamp := change.Value.Messages[0].Timestamp
 
-			response := map[string]interface{}{
-				"phoneNumberID": phoneNumberID,
-				"from":          from,
-				"msgBody":       msgBody,
+					messageResponse := map[string]interface{}{
+						"from":      from,
+						"msgBody":   msgBody,
+						"id":        id,
+						"timestamp": timeStamp,
+					}
+					messageResponses = append(messageResponses, messageResponse)
+
+					metaDataResponse := map[string]interface{}{
+						"phoneNumberID":      phoneNumberID,
+						"displayPhoneNumber": DisplayPhoneNumber,
+					}
+
+					metaDataResponses = append(metaDataResponses, metaDataResponse)
+				}
+
+				if len(change.Value.Contacts) > 0 {
+					for _, contact := range change.Value.Contacts {
+						profileName := contact.Profile.Name
+						waID := contact.WaID
+
+						contactResponse := map[string]interface{}{
+							"profileName": profileName,
+							"waID":        waID,
+						}
+						contactResponses = append(contactResponses, contactResponse)
+					}
+				}
 			}
-			jsonResponse, err := json.Marshal(response)
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return
-			}
-			w.Header().Set("Content-Type", "application/json")
-			w.WriteHeader(http.StatusOK)
-			w.Write(jsonResponse)
-			return
-			
+		}
 
-			// Implement your logic to send a response back
-			// Note: You need to implement the functionality to send a message back to the sender.
-			// For this example, we're just sending a 200 OK response.
-			w.WriteHeader(http.StatusOK)
+		jsonResponse, err := json.Marshal(map[string]interface{}{
+			"metadata": metaDataResponses,
+			"messages": messageResponses,
+			"contacts": contactResponses,
+		})
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
+
+		err = webhook.InsertWhatsappMsgData(db, jsonResponse)
+		if err != nil {
+			fmt.Println("Error inserting data:", err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(jsonResponse)
+	} else {
+		fmt.Println("Invalid payload")
+		w.WriteHeader(http.StatusNotFound)
 	}
-
-	fmt.Println("Invalid payload")
-	w.WriteHeader(http.StatusNotFound)
 }
-
 func StartWebhookServer(db *sql.DB) {
-	http.HandleFunc("/webhook", webhook.WebhookHandler(webhookHandler))
+	http.HandleFunc("/webhook", webhook.WebhookHandler(webhookHandler, db))
 	log.Println("Webhook server started")
 	log.Fatal(http.ListenAndServe(":3000", nil))
 }
--- webhook/meta_webhook.go
+++ webhook/meta_webhook.go
@@ -1,36 +1,55 @@
 package webhook
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/google/uuid"
 )
 
 // HandlerFunc is a function type for handling webhook requests
-type HandlerFunc func(http.ResponseWriter, *http.Request, WebhookPayload)
+type HandlerFunc func(http.ResponseWriter, *http.Request, *sql.DB, WebhookPayload)
 
-// WebhookPayload represents the structure of the webhook payload
 type WebhookPayload struct {
 	Object string `json:"object"`
 	Entry  []struct {
+		ID      string `json:"id"`
 		Changes []struct {
 			Value struct {
+				MessagingProduct string `json:"messaging_product"`
+				Metadata         struct {
+					DisplayPhoneNumber string `json:"display_phone_number"`
+					PhoneNumberID      string `json:"phone_number_id"`
+				} `json:"metadata"`
+				Contacts []struct {
+					Profile struct {
+						Name string `json:"name"`
+					} `json:"profile"`
+					WaID string `json:"wa_id"`
+				} `json:"contacts"`
 				Messages []struct {
-					From string `json:"from"`
-					Text struct {
+					From      string `json:"from"`
+					ID        string `json:"id"`
+					Timestamp string `json:"timestamp"`
+					Text      struct {
 						Body string `json:"body"`
 					} `json:"text"`
+					Type     string `json:"type"`
 					Metadata struct {
-						PhoneNumberID string `json:"phone_number_id"`
+						DisplayPhoneNumber string `json:"display_phone_number"`
+						PhoneNumberID      string `json:"phone_number_id"`
 					} `json:"metadata"`
 				} `json:"messages"`
 			} `json:"value"`
+			Field string `json:"field"`
 		} `json:"changes"`
 	} `json:"entry"`
 }
 
 // WebhookHandler creates a handler function for webhook requests
-func WebhookHandler(handler HandlerFunc) http.HandlerFunc {
+func WebhookHandler(handler HandlerFunc, db *sql.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var payload WebhookPayload
 		err := json.NewDecoder(r.Body).Decode(&payload)
@@ -40,6 +59,35 @@ func WebhookHandler(handler HandlerFunc) http.HandlerFunc {
 			return
 		}
 
-		handler(w, r, payload)
+		handler(w, r, db, payload)
 	}
+}
+
+func InsertWhatsappMsgData(db *sql.DB, jsonData []byte) error {
+	gid := uuid.New()
+
+	var data map[string]interface{}
+	err := json.Unmarshal(jsonData, &data)
+	if err != nil {
+		return err
+	}
+
+	messages := data["messages"].([]interface{})
+	message := messages[0].(map[string]interface{})
+	from := message["from"].(string)
+
+	// take when need (will apply based on logic)
+	// msgBody := message["msgBody"].(string)
+	// timestamp := message["timestamp"].(string)
+
+	// // Extracting data from contacts and metadata
+	// contacts := data["contacts"].([]interface{})
+	// metadata := data["metadata"].([]interface{})
+	// phoneNumber := metadata[0].(map[string]interface{})["displayPhoneNumber"].(string)
+
+	// _, err = db.Exec("INSERT INTO whatsapp_data (gid, sender_phone_number, message_body, message_timestamp, display_phone_number) VALUES ($1, $2, $3, $4, $5)", gid, from, msgBody, timestamp, phoneNumber)
+	// return err
+
+	_, err = db.Exec("INSERT INTO whatsapp_data (gid,sender_phone_number, message_data) VALUES ($1,$2,$3)", gid, from, string(jsonData))
+	return err
 }
